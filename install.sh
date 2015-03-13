dc88a6b0efb6c3c1ecbdf7004c9330ed98578ef3
Dariusz Smigiel (dasm) <dariusz.smigiel@intel.com>
2015-03-13 19:19:52 +0100

    Added readme

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+function link_file {
+    source="${PWD}/$1"
+    target="${HOME}/${1/_/.}"
+
+    if [ -L "${target}" ]; then
+        unlink $target
+    fi
+
+    if [ -e "${target}" ] && [ ! -L "${target}" ]; then
+        mv $target $target.bak
+    fi
+
+    ln -sf ${source} ${target}
+}
+
+function unlink_file {
+    source="${PWD}/$1"
+    target="${HOME}/${1/_/.}"
+
+    if [ -e "${target}.bak" ] && [ -L "${target}" ]; then
+        unlink ${target}
+        mv $target.bak $target
+    fi
+}
+
+if [ "$1" == "undeploy" ]; then
+    for i in _*
+    do
+        unlink_file $i
+    done
+fi
+
+if [ "$1" == "deploy" ]; then
+    for i in _*
+    do
+        link_file $i
+    done
+fi
